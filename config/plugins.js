508aa352a1e2ca0ff382df369cf00e8de9e98b9b
Cameron Preston <cameron@shopvida.com>
2023-03-11 19:22:42 -0800

    integrating sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -3,5 +3,17 @@ module.exports = ({ env }) => ({
     config: {
       jwtSecret: env('JWT_SECRET'),
     }
-  }
+  },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'no-reply@steward.garden',
+        defaultReplyTo: 'no-reply@steward.garden',
+      },
+    },
+  },
 })
