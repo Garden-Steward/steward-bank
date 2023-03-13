3f0fdc85b34592f7bdace05594cd12922b945836
Cameron Preston <cameron@shopvida.com>
2023-03-12 21:05:41 -0700

    updating find to just be usable for Administrators to see all the gardens otherwise filter

--- src/api/garden/controllers/garden.js
+++ src/api/garden/controllers/garden.js
@@ -9,7 +9,7 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::garden.garden', ({ strapi }) => ({
   // Method 1: Creating an entirely custom action
   async fullSlug(ctx) {
-
+    console.log(ctx.params, ctx.query)
     const entity = await strapi.db.query('api::garden.garden').findOne({
       where: {slug: ctx.params.slug},
       populate: { recurring_tasks: true, volunteers:true }
@@ -22,5 +22,30 @@ module.exports = createCoreController('api::garden.garden', ({ strapi }) => ({
     } catch (err) {
       ctx.body = err;
     }
+  }, 
+  
+  async find(ctx) {
+    if (ctx.state.user.role.type !== 'administrator') {
+      console.log(ctx.state.user)
+
+      const gardens = await strapi.db.query('api::garden.garden').findMany({
+        where: {
+          volunteers: ctx.state.user.id
+        },
+        populate: ['volunteers']
+      });
+
+      // CAUTION: SANITIZING LOSES POPULATED
+      // const sanitizedResults = await this.sanitizeOutput(gardens, ctx);
+      return this.transformResponse(gardens);
+    }
+        
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    // some more custom logic
+    meta.date = Date.now()
+    meta.test = "ADMINISTRATOR STUFF YAY";
+
+    return { data, meta };
   }
 }));
