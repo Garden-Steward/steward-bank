a85a2573838f8fa59c33833981cd5073280e4ac3
Cameron Preston <cameron@shopvida.com>
2023-06-02 10:03:20 -0700

    spacing updates

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -12,13 +12,13 @@ module.exports = {
 
   /**“At minute 0 past every 3rd hour from 1 through 23.” */
   '0 1/3 * * *': async () => {
-  // '1/1 * * * *': async () => {
+    // '1/1 * * * *': async () => {
     Weather.runWeatherCron();
   },
 
   /** Create Tasks from Recurring Tasks - add Scheduler Volunteers if any */
   '0 7 * * */1': async () => {
-  // '1/1 * * * *': async () => {
+    // '1/1 * * * *': async () => {
     const recurringTasks = await strapi.service('api::recurring-task.recurring-task').getRecurringTaskGarden();
     let curTask, recTask;
     try {
@@ -27,11 +27,11 @@ module.exports = {
         // If at least one of this type of recurring task is Initialized, skip
         curTask = await strapi.db.query('api::garden-task.garden-task').findOne({
           where: {
-            status:{$notIn: ['FINISHED', 'SKIPPED', 'ABANDONED']}, //$notIn: ['Hello', 'Hola', 'Bonjour']
-            recurring_task: recTask.id, 
-            garden:recTask.garden
+            status: { $notIn: ['FINISHED', 'SKIPPED', 'ABANDONED'] }, //$notIn: ['Hello', 'Hola', 'Bonjour']
+            recurring_task: recTask.id,
+            garden: recTask.garden
           },
-          populate: { recurring_task: true, volunteers:true }
+          populate: { recurring_task: true, volunteers: true }
         });
 
         /** SCHEDULER */
@@ -45,40 +45,40 @@ module.exports = {
       console.error(err);
     }
   },
- 
+
   taskReminders: {
-    task: async({ strapi }) => {
+    task: async ({ strapi }) => {
       strapi.log.info('triggering taskReminders cron');
       try {
         await Helper.handleInitialTasks();
-    
+
         await Helper.handleStartedTasks();
       } catch (err) {
         console.log("ERR taskReminders: ", err);
       }
-  
+
     },
     options: {
-      //  rule: '1/1 * * * *',
-       rule: '2 1/2 * * *',
-       tz: 'America/Los_Angeles',
+      // rule: '1/1 * * * *',
+      rule: '2 1/2 * * *',
+      tz: 'America/Los_Angeles',
     },
   },
 
   sendVolunteerReminder: {
-    task: async({ strapi }) => {
+    task: async ({ strapi }) => {
       strapi.log.info('triggering sendVolunteerReminder cron');
       try {
         await Helper.handleVolunteerReminders();
-          } catch (err) {
+      } catch (err) {
         console.log("ERR sendVolunteerReminder: ", err);
       }
-  
+
     },
     options: {
       //  rule: '1/1 * * * *',
-       rule: '5 8 * * *',
-       tz: 'America/Los_Angeles',
+      rule: '5 8 * * *',
+      tz: 'America/Los_Angeles',
     },
   },
 };
